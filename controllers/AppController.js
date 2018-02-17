4df6fd8d8d846c939e565aafc37acc12b29adffc
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-02-17 10:45:53 +0330

    Orders validate before creating

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -23,8 +23,33 @@ class AppController {
      * Puts an sell/buy order to exchange market
      * @param {String} type - order type ("buy" | "sell")
      * @param {Object} options - order options { from: String, to: String, amount: Number, price: Number }
+     * @return {Order|Object} state object { success: Boolean, message: String } 
      */
     order (type, options) {
+
+        // validating amount
+        if (!options.amount || typeof options.amount !== 'number') return {
+            success: false,
+            message: `"amount" property must be defined and be a "number"!`
+        }
+
+        // validating from
+        if (!options.from || typeof options.from !== 'string') return {
+            success: false,
+            message: `"from" property must be defined and be a "string"!`
+        }
+
+        // validating to
+        if (!options.to || typeof options.to !== 'string') return {
+            success: false,
+            message: `"to" property must be defined and be a "string"!`
+        }
+
+        // validating price
+        if (!options.price || typeof options.price !== 'number') return {
+            success: false,
+            message: `"price" property must be defined and be a "number"!`
+        }
         
         // If order type was sell
         if (type.toLowerCase() === 'sell') {
